83c01017e25e2bc2a42915f747a03a63462f2c32
judele124 <gwtlyby00@gmail.com>
2024-09-08 16:45:52 +0300

    (INIT): create vite, tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-white px-4 sm:px-8 py-2">helow world</h1>
+    </>
+  )
+}
+
+export default App
