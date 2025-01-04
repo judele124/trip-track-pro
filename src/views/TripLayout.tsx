8d1e0b36cf25c1209f32b4c1f886e2e6044a2c8b
judele124 <gwtlyby00@gmail.com>
2025-01-04 18:50:20 +0200

    refactor bottom link and change is name to BottomNavigationBtn

--- src/views/TripLayout.tsx
+++ src/views/TripLayout.tsx
@@ -1,10 +1,9 @@
-import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import {ToggleDarkMode} from '../components/Navbar'
 import Button from "../components/ui/Button";
-import ImageLightDark from "../components/ui/ImageLightDark";
 import EditImg from '../assets/Edit_img.png'
 import wornnigImg from '../assets/wornnig-img.png'
-
+import BottomNavigationBtn from "../components/ui/BottomNavigationBtn";
 
 
 const TripLayout = () => {
@@ -28,49 +27,12 @@ const TripLayout = () => {
         <div className="absolute top-[75px] bottom-16 inset-0 -z-10 bg-secondary/20"><Outlet /></div>
 
         <div className="flex flex-row justify-around p-2 border-t-2 border-primary page-colors">
-          <LinkWIthIcon title="participants" alt="icon of group of people" to="participants"/>
-          <LinkWIthIcon title="map" alt="icon of map" to=""/>
-          <LinkWIthIcon title="chat" alt="icon of chat" ItHasAlerts={true} to="chat" />
+          <BottomNavigationBtn to="participants" notificationCount={num} />
+          <BottomNavigationBtn to="map" notificationCount={num} />
+          <BottomNavigationBtn notificationCount={num} to="chat" />
         </div>
       </div>
     );
   };
 
 export default TripLayout
-
-
-
-import groupImg from '../assets/GroupIcon.png'
-import groupBlackImg from '../assets/GroupBlackIcon.png'
-import mapImg from '../assets/mapIcon.png'
-import mapBlackImg from '../assets/mapBlackIcon.png'
-import chatImg from '../assets/chatIcon.png'
-import chatBlackImg from '../assets/chatBlackIcon.png'
-
-interface ILinkProps {
-  title: 'map' | 'participants' | 'chat'
-  alt: string
-  ItHasAlerts?: boolean
-  to:string
-}
-const LinkWIthIcon = ({ title , alt , ItHasAlerts = false , to }: ILinkProps) => {
-   
-  const details = {
-     title,
-     srcDark: title === "map" ? mapImg : title === "participants" ? groupImg : chatImg,
-     srcLight: title === "map" ? mapBlackImg : title === "participants" ? groupBlackImg : chatBlackImg,
-     alt,
-   }
-
-   return (
-  
-      <NavLink 
-        to={to}
-        className="rounded-full text-dark relative size-12 focus:bg-white dark:focus:bg-secondary p-2" 
-      >
-        <ImageLightDark {...details} />
-        {ItHasAlerts && <span className="absolute top-0 right-0 w-4 h-4 bg-primary
-             rounded-full text-white flex items-center justify-center" >3</span>}
-      </NavLink>
-   )
-}
