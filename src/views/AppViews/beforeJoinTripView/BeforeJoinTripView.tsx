60d8d697f9fb17bf31f38b8167e7741d8c34c445
judele124 <gwtlyby00@gmail.com>
2025-02-13 22:17:54 +0200

    change the style not finish it

--- src/views/AppViews/beforeJoinTripView/BeforeJoinTripView.tsx
+++ src/views/AppViews/beforeJoinTripView/BeforeJoinTripView.tsx
@@ -1,4 +1,3 @@
-import Logo from "@/components/Logo";
 import Button from "@/components/ui/Button";
 import useAxios from "@/hooks/useAxios";
 import useIdFromParamsOrNavigate from "@/hooks/useIdFromParamsOrNavigate";
@@ -7,6 +6,7 @@ import { tripGet } from "@/servises/tripService";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Types } from "trip-track-package";
+import LL from "@/assets/GroupIcon.png";
 
 export default function BeforeJoinTripView() {
   const { activate, data, status, loading, error } = useAxios({ manual: true });
@@ -22,75 +22,40 @@ export default function BeforeJoinTripView() {
   }
 
   const { name, description, reward }: Types["Trip"]["Model"] = data;
-  console.log(reward?.image);
+  console.log(reward);
 
-  // return (
-  //   <div className="flex flex-col gap-2">
-  //     <Logo />
-  //     <div className="my-4 flex flex-col gap-2">
-  //       <h4 className="text-pretty">
-  //         You're about to join the <strong>{name}</strong> trip
-  //       </h4>
-  //       <p className="max-h-[40vh] overflow-y-scroll whitespace-pre-line break-words">
-  //         {description}
-  //       </p>
-  //       {reward && reward.image && (
-  //         <>
-  //           <p className="text-sm font-semibold">Reward: {reward.title}</p>
-  //           <img
-  //             className="size-1/2 object-contain p-3"
-  //             src={reward.image as unknown as string}
-  //             alt=""
-  //           />
-  //         </>
-  //       )}
-  //     </div>
-  //     <div>
-  //       <Button primary className="mb-1 w-full">
-  //         <Link to={`${navigationRoutes.trip}?tripId=${tripId}`}>
-  //           Join the trip
-  //         </Link>
-  //       </Button>
-  //     </div>
-  //   </div>
-  // );
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex flex-1 flex-col gap-4 px-1">
-        <h1>Join the trip {name}</h1>
+    <div className="flex flex-col gap-6">
+      <h1>
+        You're invieted to join
+        <span className="text-pretty text-primary"> {name} </span>
+        trip
+      </h1>
 
-        <div className="flex-none rounded-xl bg-white/15 p-3">
-          <h5 className="-mt-5 w-fit rounded-lg bg-secondary p-1 px-2 shadow-md shadow-primary">
-            About
-          </h5>
-          <p
-            style={{ scrollbarWidth: "none" }}
-            className="mt-3 line-clamp-3 overflow-y-scroll text-sm"
-          >
-            {
-              "Join us on an unforgettable 14-day journey through the majestic Himalayas. Experience breathtaking mountain views, ancient monasteries, and vibrant local culture. Trek to Everest Base Camp and immerse yourself in Sherpa culture."
-            }
-          </p>
+      <p
+        style={{ scrollbarWidth: "none" }}
+        className="line-clamp-7 mt-2 overflow-y-scroll px-1 text-sm"
+      >
+        {description}
+      </p>
+
+      {reward && reward.image && (
+        <div className="flex w-[65%] flex-col items-center self-center rounded-xl border border-primary bg-light p-2 dark:bg-secondary">
+          <p className="p-3 text-base font-medium">{reward.title + " 🏆 "}</p>
+          <img
+            //need to change
+            src={reward.image as unknown as string}
+            alt={reward.title}
+            className="size-[65%] object-contain"
+          />
         </div>
+      )}
 
-        {reward && reward.image && (
-          <div className="flex-none rounded-xl bg-secondary p-3">
-            <h5 className="-mt-5 text-sm font-medium">Reward</h5>
-            <p className="mt-3 text-sm font-medium">{reward.title}</p>
-            <div className="relative h-32 overflow-hidden rounded-lg">
-              <img
-                src={reward.image as unknown as string}
-                alt={reward.title}
-                className="h-full w-full object-contain"
-              />
-            </div>
-          </div>
-        )}
-      </div>
-
-      <Button primary className="w-full">
-        <Link to={`${navigationRoutes.trip}?tripId=${tripId}`}>Join</Link>
-      </Button>
+      <Link to={`${navigationRoutes.trip}?tripId=${tripId}`}>
+        <Button primary className="w-full">
+          join
+        </Button>
+      </Link>
     </div>
   );
 }
