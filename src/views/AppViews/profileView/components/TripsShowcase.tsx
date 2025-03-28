82fb044eca0ee78cbed8d7dd395c76016b4574ee
NetanelSegal <segal.netanel4@gmail.com>
2025-03-28 16:05:20 +0300

    now there is a list on filter options that the creator choose from

--- src/views/AppViews/profileView/components/TripsShowcase.tsx
+++ src/views/AppViews/profileView/components/TripsShowcase.tsx
@@ -1,8 +1,10 @@
 import Button from '@/components/ui/Button';
-import useToggle from '@/hooks/useToggle';
 import useAxios from '@/hooks/useAxios';
 import { API_BASE_URL } from '@/env.config';
 import TripsList from './TripsList';
+import { useState } from 'react';
+
+const filterList = ['createdTrips', 'joinedTrips'] as const;
 
 export default function TripsShowcase() {
 	const {
@@ -22,24 +24,25 @@ export default function TripsShowcase() {
 		url: `${API_BASE_URL}/trip/user-in-participants`,
 	});
 
-	const { isOpen: showCreatedTrips, toggle: toggleShowCreatedTrip } =
-		useToggle(true);
+	const [filter, setFilter] = useState<(typeof filterList)[number]>(
+		filterList[0]
+	);
 
 	return (
 		<>
 			{/* buttons */}
 			<div className='mt-2'>
 				<Button
-					onClick={toggleShowCreatedTrip}
-					primary={showCreatedTrips}
-					className={`mr-2 rounded-lg px-2 py-1 text-sm font-normal text-white dark:text-dark ${showCreatedTrips ? 'dark:text-white' : ''}`}
+					onClick={() => setFilter('createdTrips')}
+					primary={filter === 'createdTrips'}
+					className={`mr-2 rounded-lg px-2 py-1 text-sm font-normal text-white dark:text-dark ${filter === 'createdTrips' ? 'dark:text-white' : ''}`}
 				>
 					Created trips
 				</Button>
 				<Button
-					onClick={toggleShowCreatedTrip}
-					primary={!showCreatedTrips}
-					className={`rounded-lg px-2 py-1 text-sm font-normal text-white dark:text-dark ${!showCreatedTrips ? 'dark:text-white' : ''}`}
+					onClick={() => setFilter('joinedTrips')}
+					primary={filter === 'joinedTrips'}
+					className={`rounded-lg px-2 py-1 text-sm font-normal text-white dark:text-dark ${filter === 'joinedTrips' ? 'dark:text-white' : ''}`}
 				>
 					Joined trips
 				</Button>
@@ -47,20 +50,27 @@ export default function TripsShowcase() {
 
 			{/* heading */}
 			<h3 className='py-3'>
-				{showCreatedTrips ? 'Created Trips' : 'Joined trips'}
+				{filter === 'createdTrips' ? 'Created Trips' : 'Joined trips'}
 			</h3>
 
 			<div className='no-scrollbar flex h-full w-full flex-col gap-2 overflow-y-auto'>
 				{!createdTripsLoading && (
 					<TripsList
-						data={showCreatedTrips ? createdTripsData : joinedTripsData}
-						loading={
-							showCreatedTrips ? createdTripsLoading : joinedTripsLoading
+						data={
+							filter === 'createdTrips' ? createdTripsData : joinedTripsData
 						}
-						status={showCreatedTrips ? createdTripsStatus : joinedTripsStatus}
+						loading={
+							filter === 'createdTrips'
+								? createdTripsLoading
+								: joinedTripsLoading
+						}
+						status={
+							filter === 'createdTrips' ? createdTripsStatus : joinedTripsStatus
+						}
 						hasError={
-							(showCreatedTrips ? createdTripsError : joinedTripsError) !=
-							undefined
+							(filter === 'createdTrips'
+								? createdTripsError
+								: joinedTripsError) != undefined
 						}
 					/>
 				)}
