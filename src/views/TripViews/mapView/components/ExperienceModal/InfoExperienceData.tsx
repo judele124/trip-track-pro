a8f84103cf0c61fb4edcb919b333000df678e4c3
judele124 <gwtlyby00@gmail.com>
2025-04-24 00:47:51 +0300

    update tripContext and redis when finish expirence

--- src/views/TripViews/mapView/components/ExperienceModal/InfoExperienceData.tsx
+++ src/views/TripViews/mapView/components/ExperienceModal/InfoExperienceData.tsx
@@ -1,15 +1,22 @@
 import Button from '@/components/ui/Button';
+import { useEffect } from 'react';
 import { Types } from 'trip-track-package';
 
 interface InfoExperienceDataProps {
-	data: Types['Trip']['Stop']['Experience']['Details']['Info']['Model']['data'];
+	data: Types['Trip']['Stop']['Experience']['Details']['Info']['Model'];
 	onClose: () => void;
+	finishExperience: (score: number) => void;
 }
 
 export default function InfoExperienceData({
-	data,
+	data: { data, score = 0 },
 	onClose,
+	finishExperience,
 }: InfoExperienceDataProps) {
+	useEffect(() => {
+		finishExperience(score);
+	}, []);
+
 	return (
 		<div className='flex flex-col gap-2'>
 			<p>{data.text}</p>
