14501033c09cd1a7f8cd320097d8bed5245c87dd
Netanel <128292678+NetanelSegal@users.noreply.github.com>
2025-05-16 17:08:24 +0300

    Tt 137 - Refactor available action on a trip in profile page (#97)
    * added updeate guides button
    
    * added enter trip btn
    
    * fix bug
    
    * after review
    
    * I make the code little bit more efficient
    
    * some simple writing changes
    
    * chages guides layout and design in trip detail view
    
    * refactor guides components
    
    * finished refactor TripRowOption
    
    ---------
    
    Co-authored-by: judele124 <gwtlyby00@gmail.com>

--- src/views/AppViews/profileView/hooks/useTripOption.ts
+++ src/views/AppViews/profileView/hooks/useTripOption.ts
@@ -6,30 +6,120 @@ import {
 	cancelTrip,
 	deleteTrip,
 	endTrip,
-	joinTrip,
 	leaveTrip,
 	startTrip,
 } from '@/servises/tripService';
-import { useAuthContext } from '@/contexts/AuthContext';
+import { TripStatusArray } from 'trip-track-package';
 
-export const creatorTripsOptionsActions = [
-	'start',
-	'complete',
-	'delete',
-	'cancel',
+interface IConditionProps {
+	isCreator: boolean;
+	isAlreadyParticipant: boolean;
+}
+
+interface IAction {
+	name: string;
+	label: string;
+	statuses: (typeof TripStatusArray)[number][];
+	condition: (props: IConditionProps) => boolean;
+	styles?: {
+		primary?: boolean;
+		className?: string;
+	};
+}
+
+export const actions: IAction[] = [
+	{
+		name: 'start',
+		label: 'Start',
+		statuses: ['created'],
+		condition: ({ isCreator }) => isCreator,
+		styles: {
+			primary: true,
+		},
+	},
+	{
+		name: 'complete',
+		label: 'Complete',
+		statuses: ['started'],
+		condition: ({ isCreator }) => isCreator,
+		styles: {
+			className: 'bg-green-500',
+		},
+	},
+	{
+		name: 'delete',
+		label: 'Delete',
+		statuses: ['created', 'cancelled'],
+		condition: ({ isCreator }) => isCreator,
+		styles: {
+			className: 'bg-red-500',
+		},
+	},
+	{
+		name: 'cancel',
+		label: 'Cancel',
+		statuses: ['created'],
+		condition: ({ isCreator }) => isCreator,
+		styles: {
+			className: 'bg-red-500',
+		},
+	},
+	{
+		name: 'join',
+		label: 'Join',
+		statuses: ['created', 'started'],
+		condition: ({ isAlreadyParticipant }) => !isAlreadyParticipant,
+		styles: {
+			className: 'bg-primary',
+		},
+	},
+	{
+		name: 'leave',
+		label: 'Leave',
+		statuses: ['created', 'started'],
+		condition: ({ isAlreadyParticipant }) => isAlreadyParticipant,
+		styles: {
+			className: 'bg-red-500',
+		},
+	},
+	{
+		name: 'enter',
+		label: 'Enter',
+		statuses: ['created', 'started'],
+		condition: ({ isAlreadyParticipant }) => isAlreadyParticipant,
+		styles: {
+			className: 'bg-primary',
+		},
+	},
 ] as const;
-export const joinedTripsOptionsActions = ['join', 'leave'] as const;
 
-export type ActionOption =
-	| (typeof creatorTripsOptionsActions)[number]
-	| (typeof joinedTripsOptionsActions)[number];
+type ActionName = (typeof actions)[number]['name'];
+
+interface ICheckIsValidActionProps extends IConditionProps {
+	currentStatus: (typeof TripStatusArray)[number];
+	actionName: ActionName;
+}
+
+export const checkIsValidAction = ({
+	currentStatus,
+	actionName,
+	...conditionProps
+}: ICheckIsValidActionProps): boolean => {
+	const action = actions.find((a) => a.name === actionName);
+	if (!action) {
+		return false;
+	}
+	return (
+		action.statuses.includes(currentStatus) && action.condition(conditionProps)
+	);
+};
 
 interface IUseTripOptions {
 	tripId: string;
 }
 
 interface IUseTripOptionsReturn {
-	handleActions: (action: ActionOption) => Promise<void>;
+	handleActions: (actionName: ActionName) => Promise<void>;
 }
 
 export default function useTripOption({
@@ -42,8 +132,8 @@ export default function useTripOption({
 
 	const { getCreatedTripsData } = useTripShowcase();
 
-	const handleActions = async (action: ActionOption) => {
-		switch (action) {
+	const handleActions = async (actionName: ActionName) => {
+		switch (actionName) {
 			case 'join':
 				nav(`${navigationRoutes.joinTrip}?tripId=${tripId}`);
 				break;
@@ -62,8 +152,11 @@ export default function useTripOption({
 			case 'delete':
 				await deleteTrip(activate, tripId);
 				break;
+			case 'Enter trip':
+				nav(`${navigationRoutes.map}?tripId=${tripId}`);
+				break;
 			default:
-				throw new Error(`Action ${action} doesn't exist`);
+				throw new Error(`Action ${actionName} doesn't exist`);
 		}
 
 		await getCreatedTripsData();
