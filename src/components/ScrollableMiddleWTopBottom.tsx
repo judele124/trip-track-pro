c29b471e3cc9950a7ff9797152a5f3906058d1be
Netanel <128292678+NetanelSegal@users.noreply.github.com>
2025-06-21 23:00:54 +0300

    bug-fix (#131)

--- src/components/ScrollableMiddleWTopBottom.tsx
+++ src/components/ScrollableMiddleWTopBottom.tsx
@@ -15,7 +15,7 @@ export default forwardRef<HTMLDivElement, IScrollableMiddleWTopBottomProps>(
 			<div ref={ref} className='relative flex grow flex-col overflow-hidden'>
 				{top && <div>{top}</div>}
 				{middle && (
-					<div className='relative flex grow flex-col overflow-hidden'>
+					<div className='relative flex flex-col overflow-hidden'>
 						<div className='pointer-events-none absolute z-10 h-5 w-full bg-gradient-to-b from-light to-transparent dark:from-dark'></div>
 						<div className='relative overflow-y-auto py-2'>{middle}</div>
 						<div className='pointer-events-none absolute bottom-0 z-10 h-10 w-full bg-gradient-to-t from-light to-transparent dark:from-dark'></div>
